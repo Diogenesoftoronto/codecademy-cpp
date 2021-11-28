67a418fa138dd1c327047296f90b747fe891109e
diogenesoftoronto <diogenesoft@protonmail.com>
2021-11-28 10:38:13 -0500

    First c++ program!

---
+++ hello-world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+std::cout << "hello world!\n";
+std::cout << "This is my first c++ program, I am using codecademy to learn it.";
+std::cout << "       1\n";
+std::cout << "     2 3\n";
+std::cout << "   4 5 6\n";
+std::cout << "7 8 9 10";
+return 0;
+};
+// first c++ program!
